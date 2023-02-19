a40e8362d496b153c2799e8603c834e1a2e6393e
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-02-19 15:32:21 -0500

    metodos de instancia

---
+++ 001_conceptos_elementales/003_metodos_de_instancia.py
@@ -0,0 +1,16 @@
+class Person:
+    age = 19
+
+    def __init__(self, nombre, nacionalidad):
+        self.nombre = nombre
+        self.nacionalidad = nacionalidad
+
+    def nadar(self):
+        print("im swimming")
+
+    def __str__(self):
+        return self.nombre + " " + self.nacionalidad
+
+
+nueva_persona = Person("Ponsiano De Loor", "Ecuatoriana")
+nueva_persona.nadar()
